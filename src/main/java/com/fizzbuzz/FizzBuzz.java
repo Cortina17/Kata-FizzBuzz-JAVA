da15d2555f3f7e958ebd758864c2d41e18894d71
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-07 19:51:51 +0100

    first part done and tested

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,3 +1,12 @@
 package com.fizzbuzz;
 
-public class FizzBuzz {}
+public class FizzBuzz {
+
+    public static String check_if_number_returns_string(int number){
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }else{
+            return number % 3 == 0 ? "Fizz" : number % 5 == 0 ? "Buzz" : String.valueOf(number);
+        }
+    }
+}
--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -12,21 +12,26 @@ public class FizzBuzzTest {
     }
 
     @Test public void test_fizz(){
-        // Given
-        int number = 3;
-        FizzBuzz fizz = new FizzBuzz();
-        // When
-        public void test_fizz();
-        // Then
-        assert
+        assertEquals("Fizz", FizzBuzz.check_if_number_returns_string(3));
+        assertEquals("Fizz", FizzBuzz.check_if_number_returns_string(6));
+        assertEquals("Fizz", FizzBuzz.check_if_number_returns_string(9));
     }
 
     @Test public void test_buzz(){
-        
+        assertEquals("Buzz", FizzBuzz.check_if_number_returns_string(5));
+        assertEquals("Buzz", FizzBuzz.check_if_number_returns_string(10));
+        assertEquals("Buzz", FizzBuzz.check_if_number_returns_string(25));
     }
 
     @Test public void test_fizzbuzz(){
-
+        assertEquals("FizzBuzz", FizzBuzz.check_if_number_returns_string(15));
+        assertEquals("FizzBuzz", FizzBuzz.check_if_number_returns_string(15));
+        assertEquals("FizzBuzz", FizzBuzz.check_if_number_returns_string(15));
     }
 
+    @Test public void test_system_prints_number(){
+        assertEquals("7", FizzBuzz.check_if_number_returns_string(7));
+        assertEquals("22", FizzBuzz.check_if_number_returns_string(22));
+        assertEquals("41", FizzBuzz.check_if_number_returns_string(41));
+    }
 }
